e14eb62b1e4fedc9ccee8b996aaa5a5768a881e2
yingkunm <yingkunm@umich.edu>
2021-05-11 18:21:58 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,23 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Head1 from '../components/Head/head1'
 import Feed from '../components/feed/feed'
-export default function Home() {
+
+export async function getServerSideProps(context){
+  const res = await fetch("http://localhost:3000/api/news")
+  const result = await res.json()
+  return{
+      props:{
+        data:result
+      }
+  }
+}
+export default function Home(props) {
   return (
     <div className={styles.container}>
     
     <Head1/>
     
-    <Feed/>
+    <Feed data={props.data}/>
     </div>
     
     
